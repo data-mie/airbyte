ea1cf2eeb9bbeba15544b3c28de9443bbdbd39a0
Simo Tumelius <simo@datamie.fi>
2022-09-23 12:05:28 +0300

    Add projects incremental stream

--- airbyte-integrations/connectors/source-dbt-cloud/source_dbt_cloud/source.py
+++ airbyte-integrations/connectors/source-dbt-cloud/source_dbt_cloud/source.py
@@ -56,8 +56,15 @@ class DbtCloudStream(HttpStream, ABC):
     See the reference docs for the full list of configurable options.
     """
 
-    # TODO: Fill in the url base. Required.
-    url_base = "https://example-api.com/v1/"
+    url_base = "https://cloud.getdbt.com/api/v2"
+
+    def __init__(self, config: Mapping[str, Any], **kwargs):
+        super().__init__(**kwargs)
+        self.config = config
+
+    @property
+    def account_id(self):
+        return self.config["account_id"]
 
     def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
         """
@@ -90,26 +97,8 @@ class DbtCloudStream(HttpStream, ABC):
         TODO: Override this method to define how a response is parsed.
         :return an iterable containing each record in the response
         """
-        yield {}
-
-
-class Customers(DbtCloudStream):
-    """
-    TODO: Change class name to match the table/data source this stream corresponds to.
-    """
-
-    # TODO: Fill in the primary key. Required. This is usually a unique field in the stream, like an ID or a timestamp.
-    primary_key = "customer_id"
-
-    def path(
-        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
-    ) -> str:
-        """
-        TODO: Override this method to define the path this stream corresponds to. E.g. if the url is https://example-api.com/v1/customers then this
-        should return "customers". Required.
-        """
-        return "customers"
-
+        for row in response.json()["data"]:
+            yield row
 
 # Basic incremental stream
 class IncrementalDbtCloudStream(DbtCloudStream, ABC):
@@ -138,6 +127,15 @@ class IncrementalDbtCloudStream(DbtCloudStream, ABC):
         the current state and picks the 'most' recent cursor. This is how a stream's state is determined. Required for incremental.
         """
         return {}
+
+
+class Projects(IncrementalDbtCloudStream):
+    cursor_field = "updated_at"
+    primary_key = "project_id"
+
+    def path(self, **kwargs) -> str:
+        return f"v2/accounts/{self.account_id}/projects"
+
 
 
 class Employees(IncrementalDbtCloudStream):
@@ -185,23 +183,23 @@ class Employees(IncrementalDbtCloudStream):
 class SourceDbtCloud(AbstractSource):
     def check_connection(self, logger, config) -> Tuple[bool, any]:
         """
-        TODO: Implement a connection check to validate that the user-provided config can be used to connect to the underlying API
-
-        See https://github.com/airbytehq/airbyte/blob/master/airbyte-integrations/connectors/source-stripe/source_stripe/source.py#L232
-        for an example.
+        Check connection to the API using the GetAccount endpoint.
 
         :param config:  the user-input config object conforming to the connector's spec.yaml
         :param logger:  logger object
         :return Tuple[bool, any]: (True, None) if the input config can be used to connect to the API successfully, (False, error) otherwise.
         """
+        logger.info("Checking the config")
         api_token = config["api_token"]
         account_id = config["account_id"]
+        # TODO: Use raw endpoint
         command = DbtCloudAccountGetCommand(api_token=api_token, account_id=account_id)
         response = command.execute()
         try:
             response.raise_for_status()
         except:
-            return False, response.json()["status"]["user_message"]
+            error_message = response.json()["status"]["user_message"]
+            return False, f"Unable to connect to dbt Cloud API with the provided configuration - {error_message}"
         return True, None
 
     def streams(self, config: Mapping[str, Any]) -> List[Stream]:
@@ -210,6 +208,5 @@ class SourceDbtCloud(AbstractSource):
 
         :param config: A Mapping of the user input configuration as defined in the connector spec.
         """
-        # TODO remove the authenticator if not required.
-        auth = TokenAuthenticator(token="api_key")  # Oauth2Authenticator is also available if you need oauth support
-        return [Customers(authenticator=auth), Employees(authenticator=auth)]
+        auth = TokenAuthenticator(token=config["api_token"])
+        return [Projects(config=config, authenticator=auth)]
