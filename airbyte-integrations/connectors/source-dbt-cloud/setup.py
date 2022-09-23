b4a7efe196e4456a54462c3622dbe4232a0bb77c
Simo Tumelius <simo@datamie.fi>
2022-09-23 11:39:17 +0300

    Add connection check using the GetAccount API endpoint

---
+++ airbyte-integrations/connectors/source-dbt-cloud/setup.py
@@ -0,0 +1,30 @@
+#
+# Copyright (c) 2022 Airbyte, Inc., all rights reserved.
+#
+
+
+from setuptools import find_packages, setup
+
+MAIN_REQUIREMENTS = [
+    "airbyte-cdk~=0.1.56",
+    "dbt-cloud-cli~=0.7.2"
+]
+
+TEST_REQUIREMENTS = [
+    "pytest~=6.1",
+    "pytest-mock~=3.6.1",
+    "source-acceptance-test",
+]
+
+setup(
+    name="source_dbt_cloud",
+    description="Airbyte source implementation for the dbt Cloud API.",
+    author="Simo Tumelius",
+    author_email="simo@datamie.fi",
+    packages=find_packages(),
+    install_requires=MAIN_REQUIREMENTS,
+    package_data={"": ["*.json", "*.yaml", "schemas/*.json", "schemas/shared/*.json"]},
+    extras_require={
+        "tests": TEST_REQUIREMENTS,
+    },
+)
---
+++ airbyte-integrations/connectors/source-dbt-cloud/source_dbt_cloud/__init__.py
@@ -0,0 +1,8 @@
+#
+# Copyright (c) 2022 Airbyte, Inc., all rights reserved.
+#
+
+
+from .source import SourceDbtCloud
+
+__all__ = ["SourceDbtCloud"]
